05a283c8516e790bac222324411da551b9af0e0f
Situ Corp <72412747+SituCorp@users.noreply.github.com>
2021-09-17 12:03:19 -0500

    Discilon

---
+++ discsilon/assets/js/panel/copyright.js
@@ -0,0 +1,9 @@
+﻿/**
+ * @file copyright.js
+ * @author Sanjay Sunil 
+ * @license GPL-3.0
+ */
+
+$(".000").replaceWith("Copyright © 2021");
+$(".001").replaceWith("Uzumaki");
+$(".002").replaceWith("Todos los derechos reservados.");
