4b1d4d289caaa7f8e64e7c99f28e36988ef863d1
Situ Corp <72412747+SituCorp@users.noreply.github.com>
2021-12-21 14:45:40 -0600

    Delete discsilon directory

--- discsilon/assets/js/panel/copyright.js
+++
@@ -1,9 +0,0 @@
-﻿/**
- * @file copyright.js
- * @author Sanjay Sunil 
- * @license GPL-3.0
- */
-
-$(".000").replaceWith("Copyright © 2021");
-$(".001").replaceWith("Uzumaki");
-$(".002").replaceWith("Todos los derechos reservados.");
